a786fcc682be987230709afb71a8e3d509f5b823
ryoko-saito <39354212+ryoko-saito@users.noreply.github.com>
2025-09-05 15:27:34 +0900

    Update transmitter.py
    I added a note

--- transmitter.py
+++ transmitter.py
@@ -1,12 +1,12 @@
 from microbit import *
-import radio
+import radio//無線通信のモジュールをインポート
 
 display.show(Image.ARROW_N)
 
-radio.config(group=1, power=1)
-radio.on()
-# ここにコードを書いてね :-)
+radio.config(group=1, power=1)//1グループ、信号強度1で無線を構成します
+radio.on()//無線通信をonにする
+
 i = 0
-while True:
-    radio.send(str(i))
-    display.show(Image.HEART)
+while True://ずっと
+    radio.send(str(i))//iを送信する
+    display.show(Image.HEART)//ハートを表示する
