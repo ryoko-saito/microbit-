9eb1ecb7493dece204c6d98bfd9e6b5cda7873bb
ryoko-saito <39354212+ryoko-saito@users.noreply.github.com>
2025-09-05 15:53:01 +0900

    Update sample.py
    add a note

--- sample.py
+++ sample.py
@@ -1,8 +1,8 @@
 from microbit import*
 import radio
 
-radio.config(group=1)
-radio.on()
+radio.config(group=1)//無線のグループを1に設定する
+radio.on()//無線通信をonにする
 
 mat0 = Image("00000:"
             "00000:"
@@ -23,12 +23,12 @@ mat2 = Image("00900:"
             "99999:")
 
 while True:
-    details = radio.receive_full()
-    if details != None:
-        rssi = details[1]
-        if rssi > -60:
+    details = radio.receive_full()//キューにある3つの値を返します（メッセージのバイト列、信号強度、タイムスタンプ）
+    if details != None://もしキューに何か値があれば
+        rssi = details[1]//バイト列をrssiにいれます
+        if rssi > -60://-60よりも大きな数字（1m圏内）だったら殆ど点灯
             display.show(mat2)
-        elif rssi  > -70:
+        elif rssi  > -70://-70よりも大きな数字（3M圏内）
             display.show(mat1)
         else:
             display.show(mat0)
