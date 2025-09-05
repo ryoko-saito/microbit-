2b9716218d3d8b5dbd596326572a0de19792825a
ryoko-saito <39354212+ryoko-saito@users.noreply.github.com>
2025-09-05 16:28:40 +0900

    Update transmitter.py
    add a note

--- transmitter.py
+++ transmitter.py
@@ -1,12 +1,12 @@
 from microbit import *
 import radio#無線通信のモジュールをインポート
 
-radio.config(group=1, power=1)//1グループ、信号強度1で無線を構成します
+radio.config(group=1, power=1)#1グループ、信号強度1で無線を構成します
 radio.on()#無線通信をonにする
 
 i = 0
-while True://ずっと
-    radio.send(str(i))//iを送信する
+while True:#ずっと
+    radio.send(str(i))#iを送信する
     display.scroll('i')
     i=+i
     sleep(1000) 
